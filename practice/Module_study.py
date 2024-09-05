62ab546c6ae9e0e0f3a9970fed55f51103070428
on99yu <dgjjanggu@naver.com>
2024-09-05 21:14:07 +0900

    0905 study

---
+++ practice/Module_study.py
@@ -0,0 +1,15 @@
+import pyautogui
+from time import sleep
+
+sleep(1)
+pyautogui.write('this is written by computer',interval=0.25)
+
+# import random, string, math
+# random_number = random.randint(0,10)
+# print(random_number)
+
+
+# test_list = ['hello',False, [1,2,3],4,5,6,]
+# print(random.choice(test_list))
+# print(string.ascii_lowercase)
+# print(math.sin(1))
